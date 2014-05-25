42cc6d6b020f4d37e039b2815aa758f1ea1169fe
Peter Zotov <whitequark@whitequark.org>
2014-05-25 21:46:33 +0400

    Actually implement shadow-directory.

--- lib/core/graph.ml
+++ lib/core/graph.ml
@@ -3,6 +3,8 @@ type shadow = {
   blocks   : Block.digest list            [@key 2];
 } [@@protobuf]
 
+exception Error of [ `Not_found | `Unavailable | `Malformed | `Not_supported ]
+
 let file_shadow ~convergence ~client file_capa =
   match%lwt Chunk.retrieve_data ~decoder:File.file_from_protobuf client file_capa with
   | (`Not_found | `Unavailable | `Malformed) as err -> Lwt.return err
@@ -13,7 +15,24 @@ let file_shadow ~convergence ~client file_capa =
     let shadow = { children = []; blocks; } in
     Chunk.store_data ~convergence ~encoder:shadow_to_protobuf client shadow
 
-let directory_shadow ~convergence ~client dir_capa =
-  assert false(*
-  { children = [];
-    blocks   = List.map (fun entry -> entry.Directory.content) directory } *)
+let rec directory_shadow ~convergence ~client dir_capa =
+  match%lwt Chunk.retrieve_data ~decoder:Directory.directory_from_protobuf client dir_capa with
+  | (`Not_found | `Unavailable | `Malformed) as err -> Lwt.return err
+  | `Ok directory ->
+    try%lwt
+      let%lwt children =
+        directory |> Lwt_list.map_s (fun entry ->
+          match%lwt
+            match entry.Directory.content with
+            | `File capa -> file_shadow ~convergence ~client capa
+            | `Directory capa -> directory_shadow ~convergence ~client capa
+          with
+          | (`Not_found | `Unavailable | `Malformed | `Not_supported) as err ->
+            [%lwt raise (Error err)]
+          | `Ok shadow -> Lwt.return shadow
+          | _ -> assert false)
+      in
+      let shadow = { children; blocks = [] } in
+      Chunk.store_data ~convergence ~encoder:shadow_to_protobuf client shadow
+    with Error err ->
+      Lwt.return err
