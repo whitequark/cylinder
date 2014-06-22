c39cc45f3e5d5667eb85fa7d015438cc9e80c10e
Peter Zotov <whitequark@whitequark.org>
2014-06-22 18:14:45 +0400

    Update paths in cyinder_top.

--- lib/cylinder_top.ml
+++ lib/cylinder_top.ml
@@ -3,5 +3,7 @@ let () =
   Topdirs.dir_directory "./_build/lib/core";
   Topdirs.dir_directory "./_build/lib/crypto";
   Topdirs.dir_directory "./_build/lib/backends";
+  Topdirs.dir_directory "./_build/lib/config";
+  Topdirs.dir_directory "./_build/lib/local";
   Topfind.load_deeply ["ZMQ"; "lwt"];
   UTop_main.main ()
